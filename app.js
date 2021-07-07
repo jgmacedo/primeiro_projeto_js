84cceced9b93d0c7a062a6399ec2d83daeed3a24
João Gabriel <jgabreumacedo@gmail.com>
2021-07-06 23:17:04 -0300

    corrigido problema no app.js, agora roda um console.log

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-
-//pegar input do usuário
-//se sim, mostra categorias e pergunta qual ela escolhe.
-//se não, mostra todos os livros em ordem crescente por quantidade de pgs
 const livros = require('./database')
-const readline = require("readline-sync"),
-
-const entradaInicial = readline.question("Deseja buscar um livro?S/N")
-
-// if (entradaInicial.toLocaleUpperCase() === S){
-//     console.log("essas sao as categorias disponíveis")
-//     console.log('produtividade e estilo de vida, arte e cultura, concursos públicos, arte e cultura, mangá')
-// }
+console.log(livros)
