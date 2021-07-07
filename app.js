4aeab5d618ea269555e732933f0657427442f1e0
João Gabriel <jgabreumacedo@gmail.com>
2021-07-06 23:28:32 -0300

    adicionado IF para buscar por categoria no app

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const livros = require('./database')
-console.log(livros)
+
+//pegar input do usuario se quer escolher por categoria ou n
+
+const readline = require('readline-sync')
+
+const entradaInicial = readline.question("Deseja buscar um livro?S/N")
+
+//se for sim, mostra categorias e perguntamos qual ela escolhe.
+//se não, mostrar todos os livros em ordem crescente pela quantidade de páginas
+
+if (entradaInicial.toLocaleUpperCase() === 'S'){
+    console.log("essas são as categorias disponíveis:");
+    console.log(('produtividade e estilo de vida','/arte e cultura','/concursos públicos','/mangá'))
+
+    const entradaCategoria = readline.question('Qual categoria escolhida:')
+
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno)
+}
